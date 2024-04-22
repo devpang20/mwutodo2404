6e98a2d5223c88fcb3395e8f3eddbe59788c6225
sik2 <bickwang123@gmail.com>
2024-04-22 21:08:43 +0900

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import TodoForm from './components/TodoForm'
 import TodoHeader from './components/TodoHeader'
 import TodoList from './components/TodoList'
 
 function App() {
-    const [todos, setTodos] = useState(['할일', '할일', '할일'])
+    const [todos, setTodos] = useState([
+        { id: 1, text: '할일' },
+        { id: 2, text: '할일' },
+        { id: 3, text: '할일' },
+    ])
+
+    let lastId = useRef(4)
 
     const onInsert = (text) => {
-        setTodos([...todos, text])
+        const updateTodos = todos.concat({
+            id: lastId.current,
+            text,
+        })
+
+        setTodos(updateTodos)
+        lastId.current++
+    }
+
+    const onDelete = (id) => {
+        const updateTodos = todos.filter((todo) => todo.id !== id)
+        setTodos(updateTodos)
     }
 
     return (
         <>
             <TodoHeader />
             <TodoForm onInsert={onInsert} />
-            <TodoList todos={todos} />
+            <TodoList todos={todos} onDelete={onDelete} />
         </>
     )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,13 @@
-function TodoList({ todos }) {
+function TodoList({ todos, onDelete }) {
     return (
         <>
             <ul>
                 {todos.map((todo) => (
-                    <li>{todo}</li>
+                    <>
+                        <li>
+                            {todo.id} / {todo.text} / <button onClick={() => onDelete(todo.id)}>삭제</button>
+                        </li>
+                    </>
                 ))}
             </ul>
         </>
